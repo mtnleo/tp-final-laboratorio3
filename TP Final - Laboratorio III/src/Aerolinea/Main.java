e1e345142cc36c16b4466579c7546f4c7c471c44
mtnleo <mtnleonardi@gmail.com>
2023-06-01 16:21:11 -0300

    Actualizacion

--- TP Final - Laboratorio III/src/Aerolinea/Main.java
+++
@@ -1,7 +0,0 @@
-package Aerolinea;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- TP Final - Laboratorio III/src/Aerolinea/Socio.java
+++
@@ -1,7 +0,0 @@
-package Aerolinea;
-
-public interface Socio {
-    int porcentajeDescuento;
-    double millas;
-    // Hay que pensar bien las funciones de millas y ponerlas acá (sumar, restar, usar, acumular, habría que ver).
-}
